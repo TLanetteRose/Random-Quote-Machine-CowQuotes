0f4a81a1fdc16c1d180fde63fda8e1b20a4429fa
T. Lanette <TLPollard4516@gmail.com>
2019-09-02 19:13:20 -0400

    Feat Added quotes and random function
    Updated App.js by adding random function with lodash; fetched cowquotes.json

--- src/App.js
+++ src/App.js
@@ -1,63 +1,52 @@
 import React, { Component } from 'react';
 import { random } from 'lodash';
 import './App.css';
+import Button from './component/Button';
 import QuoteMachine from './component/QuoteMachine';
 
 
 
 
 
-class App extends Component {
 
-  constructor(props) {
+class App extends Component {
+  constructor(props){
     super(props);
-    this.state = {
+    this.state= {
       quotes: [],
       selectedQuoteIndex: null,
     }
-    this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
-    this.generateNewQuoteIndex = this.generateNewQuoteIndex.bind(this);
+    this.selectQuoteIndex = this.selectQuoteIndex.bind(this);
   }
 
   componentDidMount() {
     fetch('https://gist.githubusercontent.com/TLanetteRose/cbb344a12b2586a070d1f3fa2fe02414/raw/bd6cdd24425bd0db124d05f6662dbe2a5e6fa0fc/cowquotes.json')
       .then(data => data.json())
-      .then(quotes => this.setState({ quotes }, this.assignNewQuoteIndex));
-      
+      .then(quotes => this.setState({ quotes }, () => {
+        this.setState({ selectedQuoteIndex: this.selectQuoteIndex() })
+      }));
   }
 
-  get selectedQuote () {
-    if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)){
-      return undefined;
-    }
-    return this.state.quotes[this.state.selectedQuoteIndex];
+//Use Lodash library for random function
+selectQuoteIndex() {
+  if(!this.state.quotes.length){
+    return;
   }
-
-/**
-* Returns an integer representing an index in state.quotes
-* If state.quotes is empty, returns undefined
-*/
-
-  generateNewQuoteIndex() {
-    if(!this.state.quotes.length) {
-      return undefined;
-    }
-    return random(0, this.state.quotes.length - 1);
-  }
-
-  assignNewQuoteIndex() {
-    this.setState({ selectedQuoteIndex: this.generateNewQuoteIndex() });
-  } //Method for new quote
-
+  return random(0, this.state.quotes.length - 1);
+}
 
 
 
   render() {
+    console.log(this.state.selectedQuoteIndex);
     return (
-
-        <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex} />
+      <div className="App" id="quote-box">
+        <Button buttonDisplayName="New Quote" clickHandler={this.nextQuoteClickHandler} />
+      </div>
     );
   }
+
+  
 }
 
 export default App;
