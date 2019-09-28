59e30f4c315a041afc67502bcd78691ce14800d2
T. Lanette <tlpollard4516@gmail.com>
2019-09-28 14:54:40 -0400

    Update index.js

--- service/index.js
+++ service/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from './react.js';
+import ReactDOM from './react-dom.js';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
