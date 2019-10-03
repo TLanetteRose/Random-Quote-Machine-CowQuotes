28f2313ad9991f9ec67a0abe2c28ab72ac8aca14
T. Lanette <TLPollard4516@gmail.com>
2019-10-02 20:05:51 -0400

    Updates

--- src/component/QuoteImage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Img from 'react-image';
-
-const QuoteImage = () => (
-    <Img src={require('/Users/TLanPollard/Documents/GitHub/Random-Quote-Machine-CowQuotes/src/component/RebelCowV1.jpg')}/> 
-)
-
-export default QuoteImage;
